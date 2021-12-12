9b1e7c40b6316209dab8ad633c46e2abe443a5fc
msivakumarmudili <msivakumar.mudili@gmail.com>
2021-12-12 18:03:29 +0530

    adding patch

---
+++ src/main/java/coin/Patch.java
@@ -0,0 +1,37 @@
+package coin;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class Patch {
+    private static String requestBody = "{\n" +
+            "  \"title\": \"bax\" \n}";
+
+    @BeforeAll
+    public static void setup() {
+        RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+    }
+
+    @Test
+    public void patchRequest() {
+        Response response = given()
+                .header("Content-type", "application/json")
+                .and()
+                .body(requestBody)
+                .when()
+                .patch("/posts/1")
+                .then()
+                .extract().response();
+
+        Assertions.assertEquals(200, response.statusCode());
+        Assertions.assertEquals("bax", response.jsonPath().getString("title"));
+        Assertions.assertEquals("1", response.jsonPath().getString("userId"));
+        Assertions.assertEquals("1", response.jsonPath().getString("id"));
+    }
+
+}
